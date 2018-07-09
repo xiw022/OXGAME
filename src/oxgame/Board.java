c905d4bbe9ad97c8fc5702ca0b9c0de55440d96b
brucewxh <brucewxh@brucewxhdeair.lan>
2018-07-09 15:29:20 -0700

    refactor

--- src/oxgame/Board.java
+++ src/oxgame/Board.java
@@ -1,10 +1,10 @@
-/**   
-* @Title: Board.java 
-* @Package oxgame 
+/**
+* @Title: Board.java
+* @Package oxgame
 * @Description: the value of grid
-* @author Administrator 
+* @author Administrator
 * @date 2016/3/11
-* @version V1.0   
+* @version V1.0
 */
 
 package oxgame;
@@ -12,18 +12,25 @@ package oxgame;
 import java.util.Random;
 
 
-/** 
+/**
 * @ClassName: Board
 * @Description: the class that represents the OXGame grid
 * @author Administrator
-* @date 2016/3/11 
+* @date 2016/3/11
 */
 public class Board {
      public final int GRID_SIZE;  //the size of grid
      private int[][] grid;       //the grid
      Random random;
-/** 
-* @Title: Board 
+     boolean[] isWin;
+     boolean isDrawn;
+     int[] rows;
+     int[] cols;
+     int diag;
+     int diag2;
+     int total;
+/**
+* @Title: Board
 * @Description: initial the Board
 * @param Stage  int size
 */
@@ -32,21 +39,27 @@ public class Board {
         random=new Random();//AI hit
 
         grid = new int [GRID_SIZE] [GRID_SIZE];//inital the grid
-        
+
         //inital the gird 0
-        for(int i=0;i<GRID_SIZE;i++) {  
+        for(int i=0;i<GRID_SIZE;i++) {
             for(int j=0;j<GRID_SIZE;j++) {
              grid[i][j]=0;
           }
        }
+        rows = new int[GRID_SIZE];
+        cols = new int[GRID_SIZE];
+        isWin = new boolean[2];
+        isDrawn = false;
+        diag = 0;
+        diag2 = 0;
     }
-     
-    
-/** 
-* @Title: addRandomTile 
+
+
+/**
+* @Title: addRandomTile
 * @Description: VS AI, and the Ai can add O or X automaticly
 * @param int tile
-* @return void 
+* @return void
 */
     public void addRandomTile(int tile) {
       int obj_tile=tile==1?2:1;
@@ -54,7 +67,7 @@ public class Board {
       int tx,ty;
       int [] dx=new int[]{0, 1, 1, 1, 0, -1, -1, -1};
       int [] dy=new int[]{-1, -1, 0, 1, 1, 1, 0, -1};
-      
+
       int cnt;
       for(int i=0;i<3;i++)
       {
@@ -96,15 +109,15 @@ public class Board {
               win=0;
               if(grid[i][j]==0)
               {
-                for ( int k = 0; k < 4; k++ )  // direction  
-                {  
-                    if (  w_grid[i][j][k] + w_grid[i][j][k+4] >= 2 )  
-                        win += 100;  
-                    else if ( w_grid[i][j][k] + w_grid[i][j][k+4]  == 1 )  
-                        win += 10; 
+                for ( int k = 0; k < 4; k++ )  // direction
+                {
+                    if (  w_grid[i][j][k] + w_grid[i][j][k+4] >= 2 )
+                        win += 100;
+                    else if ( w_grid[i][j][k] + w_grid[i][j][k+4]  == 1 )
+                        win += 10;
                     else
                         win+=1;
-                }  
+                }
                 ai[i][j]=win;
               }
           }
@@ -122,122 +135,163 @@ public class Board {
                   tx=i;
                   ty=j;
               }
-          }          
+          }
        }
       if(tx!=-1&&ty!=-1)
          grid[tx][ty]=tile;
-        
-    }
-    
 
-/** 
-* @Title: isDrawn 
+    }
+
+
+/**
+* @Title: isDrawn
 * @Description: judge drawn game
 * @param void
-* @return boolean 
+* @return boolean
 */
     public boolean isDrawn(){
         int count=0;
 	//get the grid cell not be hitted
-        for(int m=0;m<GRID_SIZE;m++) {			
-           for(int n=0;n<GRID_SIZE;n++) {			   
+        for(int m=0;m<GRID_SIZE;m++) {
+           for(int n=0;n<GRID_SIZE;n++) {
               if(grid[m][n]==0)
               {
                  count++;
-              }   
+              }
            }
-        } 
-        
+        }
+
         if(count==0)
             return true;//drawn
         else
             return false;//not drawn
     }
- 
-/** 
-* @Title: reSet 
+    
+    /**
+    * @Title: isDrawn REFACTORED VERSION
+    * @Description: judge drawn game
+    * @param void
+    * @return boolean
+    */
+        public boolean isDrawn2(){
+            int count=0;
+    	//get the grid cell not be hitted
+            return isDrawn;
+        }
+
+/**
+* @Title: reSet
 * @Description: reset the game
 * @param void
-* @return void 
+* @return void
 */
     public void reSet() {
-		
-        for(int i=0;i<GRID_SIZE;i++) {  		
-           for(int j=0;j<GRID_SIZE;j++) {			   
+
+        for(int i=0;i<GRID_SIZE;i++) {
+           for(int j=0;j<GRID_SIZE;j++) {
               grid[i][j]=0;
            }
         }
+        for(int i = 0; i < 2; i ++) isWin[i] = false;
+        
+        for(int i = 0; i< 3; i ++) {
+        	rows[i] = 0;
+        	cols[i] = 0;
+        }
+        diag = 0;
+        diag2 = 0;
+        total = 0;
+        isDrawn = false;
     }
-   
-/** 
-* @Title: isHit 
+
+/**
+* @Title: isHit
 * @Description: judge the xth and yth grid  can hit
 * @param int x,int y
-* @return boolean 
+* @return boolean
 */
     public boolean isHit(int x,int y) {
-		
+
         if(grid[x][y]==0)
             return true;
-        
+
         return false;
     }
-    
-  
-/** 
-* @Title: setHit 
+
+
+/**
+* @Title: setHit
 * @Description: set the O or X
 * @param int x,int y,int tile
-* @return void 
+* @return void
 */
      public void setHit(int x,int y,int tile) {
-		 
+
         if(grid[x][y]==0) {
             grid[x][y]=tile;
+            int tmp = tile == 1 ? 1 : -1;
+            rows[x] += tmp;
+            cols[y] += tmp;
+            if(x == y) diag += tmp;
+           
+            if(x + y + 1 == GRID_SIZE) diag2 += tmp;
+            
+            System.out.println("cols[x]: " + cols[x]);
+            if(rows[x] == 3 || cols[y] == 3 || diag == 3 || diag2 == 3) {
+            	//System.out.println("tile win: " +  tile);
+            	  isWin[tile - 1] = true;
+            }
+            else if(rows[x] == -3 || cols[y] == -3 || diag == -3 || diag2 == -3) {
+            	//System.out.println("tile win: " +  tile);
+            	  isWin[tile - 1] = true;
+            }
+            else if(++total == GRID_SIZE*GRID_SIZE) {
+              isDrawn = true;
+            }
         }
     }
-     
-     
-/** 
-* @Title: getGrid 
+
+
+/**
+* @Title: getGrid
 * @Description: get the Grid
 * @param void
-* @return int [][]  
+* @return int [][]
 */
      public int [][] getGrid(){
          return grid;
      }
-    
-/** 
-* @Title: isWin 
+
+/**
+* @Title: isWin
 * @Description: judge whether win
 * @param int tile
-* @return boolean 
+* @return boolean
 */
     public boolean isWin(int tile) {
         //horizontal
-		
+
         for(int i=0;i<GRID_SIZE;i++) {
-			
+
             int flag=1;
-			
+
             for(int j=0;j<GRID_SIZE;j++) {
-				
+
               if(grid[i][j]!=tile){
                   flag=0;
                   break;
               }
             }
-			
+
             if(flag==1) {
                 return true;
             }
-            
+
             //vertical
-            flag=1;  
-			
+            flag=1;
+
             for(int j=0;j<GRID_SIZE;j++) {
-				
+
               if(grid[j][i]!=tile) {
                   flag=0;
                   break;
@@ -249,19 +303,19 @@ public class Board {
         }
         //slanting
           int flag=1;
-		  
+
           for(int i=0;i<GRID_SIZE;i++) {
-			  
+
                if(grid[i][i]!=tile) {
                   flag=0;
                   break;
               }
           }
-		  
+
         if(flag==1) {
             return true;//win
         }
-        
+
           flag=1;
           for(int i=0;i<GRID_SIZE;i++) {
                if(grid[i][GRID_SIZE-1-i]!=tile) {
@@ -269,12 +323,22 @@ public class Board {
                   break;
               }
           }
-		  
+
           if(flag==1) {
                 return true;
           }
-        
+
           return false;//not win
     }
     
+    /**
+    * @Title: isWin REFACTORED VERSION
+    * @Description: judge whether win
+    * @param int tile
+    * @return boolean
+    */
+        public boolean isWin2(int tile) {
+        	  return isWin[tile - 1];
+        }
+
 }
--- src/oxgame/OXGame.java
+++ src/oxgame/OXGame.java
@@ -1,10 +1,10 @@
-/**   
-* @Title: OXGame.java 
-* @Package oxgame 
+/**
+* @Title: OXGame.java
+* @Package oxgame
 * @Description: play OX game
-* @author Administrator 
+* @author Administrator
 * @date 2016/3/11
-* @version V1.0   
+* @version V1.0
 */
 package oxgame;
 
@@ -33,68 +33,68 @@ import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-/** 
+/**
 * @ClassName: OXGame
-* @Description: the class that run the OXGame 
+* @Description: the class that run the OXGame
 * @author Administrator
-* @date 2016/3/11 
+* @date 2016/3/11
 */
 public class OXGame extends Application {
-    
+
     private final int WIDTH=100; //the width of each tile
-    //two button 
+    //two button
     Button btn_ai;   // run AI button
     Button btn_man;  //run MAN buttom
-    
+
     Stage AIWindow;   //VS  AI
     Stage manWindow;  //VS man
-    
+
     Board board;   //the grid
     StackPane sp;  //the span contains grid
     Text[][] tileTexts; // the turn text
     int gridSize=3;    //the size of grid
-    
+
     int turn;   //who's turn
     boolean isGameOver; //judge game over
-/** 
-* @Title: start 
-* @Description: start the game 
+/**
+* @Title: start
+* @Description: start the game
 * @param Stage  primaryStage
-* @return void 
+* @return void
 */
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("OXGame");
-        
+
         //intial
-        turn=1;  // whose turn 
+        turn=1;  // whose turn
         board=new Board(gridSize);  //initial grid
         tileTexts=new Text[gridSize][gridSize];
         //inital the text in grid
-         for(int i=0;i<gridSize;i++)			 
-            for(int j=0;j<gridSize;j++) {				
-                tileTexts[i][j]=new Text();              
+         for(int i=0;i<gridSize;i++)
+            for(int j=0;j<gridSize;j++) {
+                tileTexts[i][j]=new Text();
                 tileTexts[i][j].setText("");
                 tileTexts[i][j].setFont(Font.font("Times New Roman", FontWeight.BOLD, 30));
                 tileTexts[i][j].setFill(Color.rgb(134, 125, 147));
             }
-         
+
         //open AI window,and VS AI
         btn_ai=new Button();
         btn_ai.setText("VS AI");
         btn_ai.setMinWidth(150);
         //set the ActionEvent on btn_ai
-        btn_ai.setOnAction(new EventHandler<ActionEvent>() {            
+        btn_ai.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 turn=1;
                 isGameOver=false;
                 board.reSet();
                 initialAI(primaryStage); //show the window of AI
-                updateGrid();  //update grid
+                updateGrid2();  //update grid
             }
         });
-        
+
         //open Man window,and VS Man
         btn_man=new Button();
         btn_man.setText("VS MAN");
@@ -106,34 +106,54 @@ public class OXGame extends Application {
                 isGameOver=false;
                 board.reSet();
                 initialMan(primaryStage); //show the windows of MAN
-                updateGrid();  //update grid
+                updateGrid2();  //update grid
             }
          });
-       
-       
+
+
        //initial VBox
         VBox vbox=new VBox();
         vbox.setSpacing ( 100 ) ;
         vbox.setStyle ("-fx-background-color: rgb(131, 175, 155)") ;
         vbox.getChildren().addAll(btn_ai,btn_man);
         vbox.setAlignment(Pos.CENTER);
-        primaryStage.setScene(new Scene(vbox,300,300));  
+        primaryStage.setScene(new Scene(vbox,300,300));
         primaryStage.show();  //show the window
     }
-   
-/** 
-* @Title: updateGrid 
+
+/**
+* @Title: updateGrid
 * @Description: update the scene depend on the board object
 * @param void
-* @return void 
-*/      
-void updateGrid()
+* @return void
+*/
+void updateGrid(int i, int j)
 {
     //get the grid
      int [][] grid=board.getGrid();
      // show the text by the value in grid
+           if(grid[i][j]==0) {
+               tileTexts[i][j].setText("");
+           }
+           if(grid[i][j]==1) {
+               tileTexts[i][j].setText("O");
+           }
+            if(grid[i][j]==2) {
+               tileTexts[i][j].setText("X");
+           }
+}
+
+/**
+* @Title: updateGrid
+* @Description: update the scene depend on the board object
+* @param void
+* @return void
+*/
+void updateGrid2()
+{
+	int [][] grid=board.getGrid();
     for(int i=0;i<gridSize;i++) {			   
-        for(int j=0;j<gridSize;j++) {				   
+        for(int j=0;j<gridSize;j++) {
            if(grid[i][j]==0) {
                tileTexts[i][j].setText("");
            }
@@ -147,13 +167,13 @@ void updateGrid()
     }
 }
 
-   
-/** 
-* @Title: initialAI 
+
+/**
+* @Title: initialAI
 * @Description: initial AI window and open it
 * @param Stage primaryStage
-* @return void 
-*/  
+* @return void
+*/
 void initialAI(Stage primaryStage)
 {
     //initial the AI window
@@ -162,7 +182,7 @@ void initialAI(Stage primaryStage)
     // set the layout of the AI window
     VBox root=new VBox();
     GridPane pane = new GridPane();
-    pane.setAlignment(Pos.CENTER); 
+    pane.setAlignment(Pos.CENTER);
     pane.setPadding(new Insets(0, 15, 0, 15));
     //set the text that shows the O or X in layout
     for(int i=0;i<gridSize;i++)
@@ -176,9 +196,9 @@ void initialAI(Stage primaryStage)
             GridPane.setHalignment(tileTexts[i][j], HPos.CENTER);
         }
     // show the grid line
-    pane.setGridLinesVisible(true);       
-    pane.setOnMouseClicked(new EventHandler<MouseEvent>() {  
-        public void handle(MouseEvent me) {  
+    pane.setGridLinesVisible(true);
+    pane.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        public void handle(MouseEvent me) {
             //judge the game is over or not
             if(isGameOver==true)
                 return;
@@ -186,7 +206,7 @@ void initialAI(Stage primaryStage)
             int y=(int)me.getX()-15;
             int x=(int)me.getY();
             //judge the hit point whether in the grid
-            
+
             //judge the hit in the grid
             if(x>=0&&x<=300&&y>=0&&y<=300) {
                 int i=x/100;
@@ -197,8 +217,8 @@ void initialAI(Stage primaryStage)
                     return;
                 // update the grid after hitting
                 board.setHit(i, j, turn);
-                updateGrid();
-                
+                updateGrid(i, j);
+
                 //judge the man is win or not
                 if( board.isWin(turn)) {//man win
                     String str=turn==1?"O":"X";
@@ -206,7 +226,7 @@ void initialAI(Stage primaryStage)
                     Text gameOver=new Text();
                     gameOver.setText(str+" Win!");
                     gameOver.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
-                    gameOver.setFill(Color.BLACK); 
+                    gameOver.setFill(Color.BLACK);
                     root.setStyle("-fx-opacity: 0.4;");
                     sp.getChildren().add(gameOver);
                     isGameOver=true;
@@ -214,21 +234,21 @@ void initialAI(Stage primaryStage)
                 }
                 //judge is drawn or not
                if( board.isDrawn()) {//drawn game
-                    String str="Drawn Game";                        
+                    String str="Drawn Game";
                     Text gameOver=new Text();
                     gameOver.setText(str);
                     gameOver.setFont(Font.font("Times New Roman", FontWeight.BOLD, 50));
-                    gameOver.setFill(Color.BLACK);    
+                    gameOver.setFill(Color.BLACK);
                     root.setStyle("-fx-opacity: 0.4;");
                     sp.getChildren().add(gameOver);
                     isGameOver=true;
                     return;
                 }
 
-                turn=turn==1?2:1;     
+                turn=turn==1?2:1;
                 //AI hit and update the grid
                 board.addRandomTile(turn);
-                updateGrid();
+                updateGrid2();
                 //judge AI is win or not
                 if( board.isWin(turn)) {//Ai win
                     String str=turn==1?"O":"X";
@@ -236,7 +256,7 @@ void initialAI(Stage primaryStage)
                     Text gameOver=new Text();
                     gameOver.setText(str+" Win!");
                     gameOver.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
-                    gameOver.setFill(Color.BLACK);  
+                    gameOver.setFill(Color.BLACK);
                     root.setStyle("-fx-opacity: 0.4;");
                     sp.getChildren().add(gameOver);
                     isGameOver=true;
@@ -244,11 +264,11 @@ void initialAI(Stage primaryStage)
                 }
                 //judge is drawn or not
                 if( board.isDrawn()) {//drawn game
-                    String str="Drawn Game";                        
+                    String str="Drawn Game";
                     Text gameOver=new Text();
                     gameOver.setText(str);
                     gameOver.setFont(Font.font("Times New Roman", FontWeight.BOLD, 50));
-                    gameOver.setFill(Color.BLACK);   
+                    gameOver.setFill(Color.BLACK);
                     root.setStyle("-fx-opacity: 0.4;");
                     sp.getChildren().add(gameOver);
                     isGameOver=true;
@@ -259,28 +279,28 @@ void initialAI(Stage primaryStage)
 
             }
         }
-    }); 
+    });
     //add the layout
-    root.getChildren().add(pane);   
+    root.getChildren().add(pane);
     root.setAlignment(Pos.CENTER);
 
-    sp=new StackPane(); 
+    sp=new StackPane();
     sp.getChildren().add(root);
 
     Scene scene=new Scene(sp,310,301);
     AIWindow.setTitle("AI");
-    AIWindow.setScene(scene);                
-    AIWindow.setResizable(false);  
+    AIWindow.setScene(scene);
+    AIWindow.setResizable(false);
     AIWindow.show();//show window
 }
-    
-     
-/** 
-* @Title: initialMan 
+
+
+/**
+* @Title: initialMan
 * @Description: initial Man window and open it
 * @param Stage primaryStage
-* @return void 
-*/  
+* @return void
+*/
 void initialMan(Stage primaryStage) {
     //initial manwindow
     manWindow=new Stage();
@@ -290,13 +310,13 @@ void initialMan(Stage primaryStage) {
     Text yourTurn=new Text();
     yourTurn.setText("TURN: "+str);
     yourTurn.setFont(Font.font("Times New Roman", FontWeight.BOLD, 40));
-    
+
     //set the layout of MANWindow
     VBox root=new VBox();
     GridPane pane = new GridPane();
-    pane.setAlignment(Pos.CENTER); 
+    pane.setAlignment(Pos.CENTER);
     pane.setPadding(new Insets(0, 15, 0, 15));
-    
+
     //add the text that shows O or X to grid
      for(int i=0;i<gridSize;i++)
 
@@ -312,8 +332,8 @@ void initialMan(Stage primaryStage) {
      //shwo the grid lines
     pane.setGridLinesVisible(true);
 
-    pane.setOnMouseClicked(new EventHandler<MouseEvent>() {  
-        public void handle(MouseEvent me) { 
+    pane.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        public void handle(MouseEvent me) {
             //judge the game is over or not
             if(isGameOver==true)
                 return;
@@ -325,67 +345,67 @@ void initialMan(Stage primaryStage) {
                 int i=x/100;
                 int j=y/100;
                 System.out.println(i+","+j);
-                
+
                 if(!board.isHit(i, j))
                     return;
                 //updathe the value in gird cell whith is hitted
                 board.setHit(i, j, turn);
-                updateGrid();
+                updateGrid(i, j);
                 // someone is win
-                if( board.isWin(turn))  {   //win
+                if( board.isWin2(turn))  {   //win
                     String str=turn==1?"O":"X";
                     System.out.println("str Win");
                     Text gameOver=new Text();
                     gameOver.setText(str+" Win!");
                     gameOver.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
-                    gameOver.setFill(Color.BLACK);   
+                    gameOver.setFill(Color.BLACK);
                     root.setStyle("-fx-opacity: 0.4;");
                     sp.getChildren().add(gameOver);
                     isGameOver=true;
                     return;
                 }
                 //the game is drawn
-               if( board.isDrawn())//Drawn
+               if( board.isDrawn2())//Drawn
                 {
-                    String str="Drawn Game";                        
+                    String str="Drawn Game";
                     Text gameOver=new Text();
                     gameOver.setText(str);
                     gameOver.setFont(Font.font("Times New Roman", FontWeight.BOLD, 50));
-                    gameOver.setFill(Color.BLACK);    
+                    gameOver.setFill(Color.BLACK);
                     root.setStyle("-fx-opacity: 0.4;");
                     sp.getChildren().add(gameOver);
                     isGameOver=true;
                     return;
                 }
 
-                turn=turn==1?2:1;                     
-                String str=turn==1?"O":"X";      
+                turn=turn==1?2:1;
+                String str=turn==1?"O":"X";
                 yourTurn.setText("TURN: "+str);
             }
         }
-    }); 
+    });
 
-    root.getChildren().addAll(yourTurn,pane);   
+    root.getChildren().addAll(yourTurn,pane);
     root.setAlignment(Pos.CENTER);
 
-    sp=new StackPane(); 
+    sp=new StackPane();
     sp.getChildren().add(root);
 
     Scene scene=new Scene(sp,310,401);
     manWindow.setTitle("MAN");
-    manWindow.setScene(scene);                
-    manWindow.setResizable(false);  
-    manWindow.show(); 
+    manWindow.setScene(scene);
+    manWindow.setResizable(false);
+    manWindow.show();
 }
-    
-/** 
-* @Title: main 
+
+/**
+* @Title: main
 * @Description: launch the programme
 * @param String[] args
-* @return void 
-*/  
+* @return void
+*/
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
